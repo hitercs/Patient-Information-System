22c31f2cf1d503d64156dc3fd37761f93293ebdd
CHENShuang1994 <979524237@qq.com>
2016-05-23 00:12:26 +0800

    fiex session error

--- doctorsMain.php
+++ doctorsMain.php
@@ -1,36 +1,47 @@
+<?php
+	session_start();
+?>
 <html>
 <header>
 	<h1>Doctors Main Page </h1>
 	<?php
-		//connect database
-		$serverName = "573dc08a9d3d2.bj.cdb.myqcloud.com";
-		$serverPort = "14376";
-		$password = "patient+infor";
-		$username = "root";
-		$db_name = "test";
-		$conn = new mysqli($serverName, $username, $password, $db_name, $serverPort);
-		if ($conn->connect_error)
+		if (!isset($_SESSION["email"]))
 		{
-			die("Connection failed: " . $conn->connect_error);
-		}
-		echo "Connected successfully";
-		// query processing 
-		$sql = "select tb_doctors.PID from tb_doctors, tb_patients where tb_doctors.Did = tb_patients.Did AND tb_doctors.email = $_SESSION['email']";
-		$result = $conn->query(sql);
-		if ($result->num_rows > 0)
-		{
-			echo "Patients ID:" . "</br>";
-			while($row = $result->fetch_assoc())
-			{
-				echo $row["PID"]."</br>";
-			}
+			$error_msg = "Request Forbidden！Please Log in first\n";
+			trigger_error($error_msg);
 		}
 		else
 		{
-			echo "No Patients~~";
+			//connect database
+			$serverName = "573dc08a9d3d2.bj.cdb.myqcloud.com";
+			$serverPort = "14376";
+			$password = "patient+infor";
+			$username = "root";
+			$db_name = "test";
+			$conn = new mysqli($serverName, $username, $password, $db_name, $serverPort);
+			if ($conn->connect_error)
+			{
+				die("Connection failed: " . $conn->connect_error);
+			}
+			echo "Connected successfully";
+			// query processing 
+			$sql = "SELECT tb_patients.PID FROM tb_doctors, tb_patients WHERE tb_doctors.Did = tb_patients.Did AND tb_doctors.email = '$_SESSION[email]'";
+			$result = $conn->query($sql);
+			if ($result->num_rows > 0)
+			{
+				echo "Patients ID:" . "</br>";
+				while($row = $result->fetch_assoc())
+				{
+					echo $row["PID"]."</br>";
+				}
+			}
+			else
+			{
+				echo "No Patients~~";
+			}
+			echo "Connected Close";
+			$conn->close();
 		}
-		echo "Connected Close";
-		$conn->close();
 	?>
 </header>
 <body>
