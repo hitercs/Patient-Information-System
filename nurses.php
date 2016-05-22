983456a3ffff1f84369715eb499d1e879ee73a4e
CHENShuang1994 <979524237@qq.com>
2016-05-22 10:27:10 +0800

    Patients Docotors Nursess Logging in

--- nurses.php
+++ nurses.php
@@ -21,7 +21,7 @@
     <![endif]-->
 </header>
     <div class="container">
-      <form class="form-signin" action="patientsLogging.php">
+      <form class="form-signin" action="nursesLogging.php">
         <h2 class="form-signin-heading">Welcome to Patients information System!</h2>
         <label for="inputEmail" class="sr-only">Email address</label>
         <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
