c308fa05b098b39e34b64a031848be87a821e5d6
CHENShuang1994 <979524237@qq.com>
2016-05-23 10:14:39 +0800

    doctor mainpage tables added

--- doctorsMain.php
+++ doctorsMain.php
@@ -3,6 +3,15 @@
 ?>
 <html>
 <header>
+	<!-- Bootstrap theme -->
+    <link href="bootstrap-3.3.5-dist/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Bootstrap theme -->
+    <link href="bootstrap-3.3.5-dist/css/bootstrap-theme.min.css" rel="stylesheet">
+	<!-- Custom styles for this template -->
+    <link href="mycss/theme.css" rel="stylesheet">
+	<!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="myjs/ie-emulation-modes-warning.js"></script>
 	<h1>Doctors Main Page </h1>
 	<?php
 		if (!isset($_SESSION["email"]))
@@ -24,29 +33,55 @@
 				die("Connection failed: " . $conn->connect_error);
 			}
 			echo "Connected successfully";
-			// query processing 
-			$sql = "SELECT tb_patients.PID FROM tb_doctors, tb_patients WHERE tb_doctors.Did = tb_patients.Did AND tb_doctors.email = '$_SESSION[email]'";
-			$result = $conn->query($sql);
-			if ($result->num_rows > 0)
-			{
-				echo "Patients ID:" . "</br>";
-				while($row = $result->fetch_assoc())
-				{
-					echo $row["PID"]."</br>";
-				}
-			}
-			else
-			{
-				echo "</br>";
-				echo "No Patients~~";
-				echo "</br";
-			}
-			echo "Connected Close";
-			$conn->close();
 		}
 	?>
 </header>
-<body>
-
+<body>	
+	<div class="page-header">
+        <h1>My patients</h1>
+    </div>
+	<div class="row">
+        <div class="col-md-6">
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Patients ID</th>
+                <th>Patients name</th>
+                <th>In date</th>
+              </tr>
+            </thead>
+            <tbody>
+				<?php
+					// query processing 
+					$sql = "SELECT tb_patients.PID, tb_patients.name, tb_patients.indate FROM tb_doctors, tb_patients WHERE tb_doctors.Did = tb_patients.Did AND tb_doctors.email = '$_SESSION[email]'";
+					$result = $conn->query($sql);
+					if ($result->num_rows > 0)
+					{
+						echo "Patients ID:" . "</br>";
+						$n = 0;
+						while($row = $result->fetch_assoc())
+						{
+							echo "<tr>";
+							echo "<td>".$n++."</td>";
+							echo "<td>".$row["PID"]."</td>";
+							echo "<td>".$row["name"]."</td>";
+							echo "<td>".$row["indate"]."</td>";
+							echo "</tr>";
+						}
+					}
+					else
+					{
+						//echo "</br>";
+						//echo "No Patients~~";
+						//echo "</br>";
+					}
+					//echo "Connected Close";
+					$conn->close();
+				?>
+            </tbody>
+          </table>
+        </div>
+	</div>
 </body>
 </html>
